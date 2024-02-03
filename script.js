c0e6bad3411f66fc2808a6207a7281070d5f7584
Adarsh Mishra <40768847+iamadarsh1994@users.noreply.github.com>
2024-02-03 22:28:10 +0000

    just starting to create it (not good)

---
+++ script.js
@@ -0,0 +1,8 @@
+apik = "0e67db94468f659ff91f9a7fffe92ba9"
+
+
+btn.addEventListener('click', function () {
+    fetch(https://api.openweathermap.org/data/3.0/onecall?lat={28.722160}&lon={77.303574}&exclude={part}&appid={"0e67db94468f659ff91f9a7fffe92ba9"})
+    .then(res => res.json())
+    .then(data =>{})
+}
